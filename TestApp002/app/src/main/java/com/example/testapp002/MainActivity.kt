2c66a3acbdd9a978c11f0bdaf5a7084600cab8e5
litdschklang <litds@outlook.com>
2021-01-28 12:10:44 +0100

    second commit (works) lesson learned: 1. in AMD "wipe data" to avoid emulator error 2. use demand "findViewById<xx>(R.id.xx)"

--- TestApp002/app/src/main/java/com/example/testapp002/MainActivity.kt
+++ TestApp002/app/src/main/java/com/example/testapp002/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.testapp002
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.GoToAddProduct).setOnClickListener{
+            startActivity(Intent(this,AddProductActivity::class.java))
+        }
+
+        val preferences = getSharedPreferences("database", Context.MODE_PRIVATE)
+        val savedName = preferences.getString("savedProductName","this value doesn't exist")
+        d("ying","saved message is: $savedName")
     }
 }
